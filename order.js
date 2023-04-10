31e87186102c8af1d03acf8c0e9386b24a4caf0a
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2023-04-10 13:23:01 +0300

    first commit

---
+++ order.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName : 'Larvender Drone',
+        productNumber: '36631',
+        paymentStatus:'Refund',
+        shipping:'Declined',
+    },
+    {
+        productName : 'Ruko F11 Pro Drone',
+        productNumber: '49631',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName : 'Drone with Camera Drone',
+        productNumber: '96996631',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    {
+        productName : 'GPS 4k Drone',
+        productNumber: '22631',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    
+]
